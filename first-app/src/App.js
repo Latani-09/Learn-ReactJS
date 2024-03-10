78d055a838f1760738988b19cd5cb0f7fe5f3b85
Latani-09 <127345009+Latani-09@users.noreply.github.com>
2024-03-10 13:07:32 +0530

    Re initiated

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,8 +1,30 @@
+
 import './App.css';
 
 function App() {
   return (
-    <h1>Hello world</h1>
+    <div>
+    <h1>Hello World</h1>
+
+    {/** 
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+    */}
+    </div>
   );
 }
 
